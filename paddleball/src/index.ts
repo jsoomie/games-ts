7800c598c2467aa1a0add17cec129e241a6376b3
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-06 14:19:47 -0700

    feat: add borders for ball to move in x and y axis and bounce

--- paddleball/src/index.ts
+++ paddleball/src/index.ts
@@ -1,6 +1,9 @@
 global.onload = function () {
   // Game stats
-  let ballX = 75;
+  let ballX = 75; // X-AXIS
+  let ballY = 75; // Y-AXIS
+  let ballSpeedX = 5; // X-AXIS SPEED
+  let ballSpeedY = 7; // Y-AXIS SPEED
 
   // Game setup
   const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
@@ -8,8 +11,22 @@ global.onload = function () {
 
   // Updates page @ 30 frames a second
   const updateAll = () => {
-    ballX += 5;
-    console.log(ballX);
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+    // BALL X-AXIS BORDERS
+    if (ballX > canvas.width) {
+      ballSpeedX *= -1;
+    } else if (ballX < 0) {
+      ballSpeedX *= -1;
+    }
+
+    // BALL Y-AXIS BORDERS
+    if (ballY > canvas.height) {
+      ballSpeedY *= -1;
+    } else if (ballY < 0) {
+      ballSpeedY *= -1;
+    }
 
     // Board
     context.fillStyle = "black";
@@ -18,7 +35,7 @@ global.onload = function () {
     // Ball
     context.fillStyle = "white";
     context.beginPath();
-    context.arc(ballX, 100, 10, 0, Math.PI * 2, true);
+    context.arc(ballX, ballY, 10, 0, Math.PI * 2, true);
     context.fill();
   };
 
