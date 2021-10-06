99a5399c352bfb2652340dbf82a90e8e4563bbd0
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-06 11:56:09 -0700

    refactor: add typeguard to canvasContext instead of using non-null assertion

--- paddleball/src/index.ts
+++ paddleball/src/index.ts
@@ -2,11 +2,15 @@ window.onload = function () {
   const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
   const canvasContext = canvas.getContext("2d");
 
-  canvasContext!.fillStyle = "black";
-  canvasContext!.fillRect(0, 0, canvas.width, canvas.height);
+  if (canvasContext) {
+    canvasContext.fillStyle = "black";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
-  canvasContext!.fillStyle = "white";
-  canvasContext!.beginPath();
-  canvasContext!.arc(100, 100, 10, 0, Math.PI * 2, true);
-  canvasContext!.fill();
+    canvasContext.fillStyle = "white";
+    canvasContext.beginPath();
+    canvasContext.arc(100, 100, 10, 0, Math.PI * 2, true);
+    canvasContext.fill();
+  } else {
+    console.log("error creating canvas and its contexts");
+  }
 };
