89af88c15887e30318193dcb77cdef503d7e9680
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-06 11:52:08 -0700

    feat: create blank canvas with ball

--- paddleball/src/index.ts
+++ paddleball/src/index.ts
@@ -1,1 +1,12 @@
+window.onload = function () {
+  const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
+  const canvasContext = canvas.getContext("2d");
 
+  canvasContext!.fillStyle = "black";
+  canvasContext!.fillRect(0, 0, canvas.width, canvas.height);
+
+  canvasContext!.fillStyle = "white";
+  canvasContext!.beginPath();
+  canvasContext!.arc(100, 100, 10, 0, Math.PI * 2, true);
+  canvasContext!.fill();
+};
